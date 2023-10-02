ce0827acb9301cf8bd5c5c3c001deee39de7c966
k.hajizadeh <k.hajizadeh@DPkharazmi.com>
2023-10-02 17:19:04 +0330

    gh-portfolio last version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
+import { Title } from '@angular/platform-browser';
+import { Meta } from '@angular/platform-browser';
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gh-portfolio';
+  username = environment.username;
 }
